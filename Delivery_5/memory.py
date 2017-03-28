a3287b6b1fa8bcbd953ebbb123bf0de7a3ae5aae
pauescalantec <pauescalante8@gmail.com>
2017-03-28 13:27:14 -0600

    Delivery 5 folder

---
+++ Delivery_5/memory.py
@@ -0,0 +1,81 @@
+"""Module for memory handling, contains memory implementations,
+memory handling, and other functions as detailed in the class"""
+class Memory_Handler:
+	def __init__(self):
+		#counter for constant memory
+		self._constant_memory = [0,0,0,0]
+		#counter for variable memory
+		self._variable_memory = [0,0,0,0]
+		#counter for function memory
+		self._function_memory = 0
+
+	#Get memory address of block
+	def _get_memory_address_block(self):
+		#memory address for block
+		address = (100+self._function_memory)
+		self._function_memory = self._function_memory+1
+		return address
+
+	#Get memory address of block
+	def _get_memory_address_variable(self, variable_type = None):
+		#variable_type should have value
+		if variable_type is not None:
+			#memory address for whole
+			if(variable_type == "whole"):
+				address = (200+self._variable_memory[0])
+				self._variable_memory[0] = self._variable_memory[0]+1
+				return address
+
+			#memory address for decimal
+			elif(variable_type == "decimal"):
+				address = (300+self._variable_memory[1])
+				self._variable_memory[1] = self._variable_memory[1]+1
+				return address
+
+			#memory address for words
+			elif(variable_type == "words"):
+				address = (400+self._variable_memory[2])
+				self._variable_memory[2] = self._variable_memory[2]+1
+				return address
+
+			#memory address for boolean
+			elif(variable_type == "boolean"):
+				address = (500+self._variable_memory[3])
+				self._variable_memory[3] = self._variable_memory[3]+1
+				return address
+
+			#does not recognize variable type
+			else:
+				return -1
+
+	#Get memory address of block
+	def _get_memory_address_constant(self, constant_type = None):
+		#constant_type should have value
+		if constant_type is not None:
+			#memory address for constant whole
+			if(constant_type == "cst_whole"):
+				address = (600+self._constant_memory[0])
+				self._constant_memory[0] = self._constant_memory[0]+1
+				return address
+
+			#memory address for constant decimal
+			elif(constant_type == "cst_decimal"):
+				address = (700+self._constant_memory[1])
+				self._constant_memory[1] = self._constant_memory[1]+1
+				return address
+
+			#memory address for constant words
+			elif(constant_type == "cst_words"):
+				address = (800+self._constant_memory[2])
+				self._constant_memory[2] = self._constant_memory[2]+1
+				return address
+
+			#memory address for constant boolean
+			elif(constant_type == "cst_boolean"):
+				address = (900+self._constant_memory[3])
+				self._constant_memory[3] = self._constant_memory[3]+1
+				return address
+
+			#does not recognize constant type
+			else:
+				return -1
---
+++ Delivery_5/scanner.py
@@ -0,0 +1,91 @@
+import ply.lex as lex
+import globalScope
+
+#List of reserved keywords
+#keyword : tokenName
+reserved = {
+    'block' : 'block',
+    'starting' : 'starting',
+    'receives' : 'receives',
+    'returns' : 'block_returns',
+    'return' : 'return_statement',    
+    'call' : 'call',
+    'variable' : 'variable',
+    'list' : 'list',
+    'of' : 'of',
+    'type' : 'type',
+    'do' : 'do',
+    'until' : 'until',
+    'if' : 'if',
+    'else' : 'else',
+    'input' : 'input',
+    'print' : 'print',
+    'whole' : 'whole',
+    'decimal' : 'decimal',
+    'words' : 'words',
+    'boolean' : 'boolean',
+    'and' : 'op_and',
+    'or' : 'op_or',
+    'not' : 'op_negation'
+}
+
+#Token list
+tokens = [
+     'colon', 'semicolon', 'comma','curlybraces_open', 'curlybraces_close', 'parenthesis_open', 
+     'parenthesis_close', 'squarebracket_open', 'squarebracket_close', 'op_assign', 'op_less', 
+     'op_less_equal', 'op_greater', 'op_greater_equal', 'op_equal', 'op_not_equal', 
+     'op_addition', 'op_subtraction', 'op_multiplication', 'op_division', 
+     'cst_whole', 'cst_decimal', 'cst_words', 'cst_boolean', 'id'] + list(reserved.values())
+
+#Token regular expressions
+t_colon = r':'
+t_semicolon = r';'
+t_comma = r','
+t_curlybraces_open = r'{'
+t_curlybraces_close = r'}'
+t_parenthesis_open = r'\('
+t_parenthesis_close = r'\)'
+t_squarebracket_open = r'\['
+t_squarebracket_close = r'\]'
+t_op_less = r'<'
+t_op_less_equal = r'<='
+t_op_greater = r'>'
+t_op_greater_equal = r'>='
+t_op_equal = r'=='
+t_op_assign = r'='
+t_op_not_equal = r'!='
+t_op_addition = r'\+'
+t_op_subtraction = r'-'
+t_op_multiplication = r'\*'
+t_op_division = r'\/'
+t_ignore  = ' \t'
+
+def t_cst_decimal(t): 
+    r'[0-9]+\.[0-9]+([Ee][\+-]?[0-9]+)?'
+    t.value = float(t.value)
+    return t
+def t_cst_whole(t):
+    r'[0-9]+'
+    t.value = int(t.value)
+    return t
+def t_cst_words(t): 
+    r'\"([^\\\"]|\\.)*\"'
+    t.type = reserved.get(t.value, 'cst_words')
+    return t
+def t_cst_boolean(t):
+    r'true|false'
+    t.value = bool(t.value)
+    return t     
+def t_id(t): 
+    r'[A-Za-z][A-Za-z0-9]*'
+    t.type = reserved.get(t.value, 'id')
+    return t 
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+    globalScope.line_count = t.lexer.lineno
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)        
+
+lexer = lex.lex()
