190b22fe3d68e59318b26d9dec8e17ee5f0c9e00
pauescalantec <pauescalante8@hotmail.com>
2017-04-30 13:36:42 -0500

    merge confilcts

--- Delivery_8/choo_choo_train.py
+++ Delivery_8/choo_choo_train.py
@@ -4,29 +4,6 @@ import virtual_machine
 import global_scope
 import time
 
-<<<<<<< Updated upstream
-#If no argument was given, make the user input a file
-if(len(sys.argv) < 2):
-    file_name = raw_input('Input the name of the file to run: ')
-#If an argument was given, read that file
-else:
-    file_name = sys.argv[1]
-    
-#counts execution time
-start_time = time.time()
-
-compilation_time = time.time() - start_time
-
-parser.start_compilation(file_name)
-virtual_machine.start_execution()
-
-#finished execution
-execution_time = time.time() - global_scope.timer_counter - start_time
-print compilation_time, "seconds"
-print execution_time, "seconds"
-=======
-#must reload virtual machine
-
 class attributes:  
     #index 0
     code = ""
@@ -84,4 +61,3 @@ def compile_and_run():
 
     print compilation_time, "seconds"
     print execution_time, "seconds"
->>>>>>> Stashed changes
